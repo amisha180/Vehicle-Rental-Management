4e6bcf6b3012da20cc33881dd2ed87740d54030a
Amisha Kumari <amishakumari47939@gmail.com>
2024-06-27 22:06:59 +0530

    Initial commit

---
+++ discoveryservice/src/test/java/com/spiro/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiro.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
