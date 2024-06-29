32d707e85cd7968f8e838eefdc146496ca4584c8
Amisha Kumari <amishakumari47939@gmail.com>
2024-06-29 21:22:39 +0530

    Update calculate_feature.java

--- configserver/src/main/resources/config/Features/calculate_feature.java
+++ configserver/src/main/resources/config/Features/calculate_feature.java
@@ -1,5 +1,5 @@
 public static void main(String[] args) {
     system.out.prinln("git is running");
     system.out.prinln("commit from master");
-    
+    system.out.prinln("commit from master");
 }
