08f4c30cabe9d7f10b2085dba709af9f4530f8bd
Amisha Kumari <amishakumari47939@gmail.com>
2024-06-29 21:37:44 +0530

    deleted unecessary files

--- configserver/src/main/resources/config/Features/developfeature.java
+++
@@ -1,4 +0,0 @@
-public static void main(String[] args) {
-    System.out.println("this is important fix feature for develop");
-    System.out.println("this is important fix feature for develop");
-}
--- configserver/src/main/resources/config/feature.java
+++
@@ -1,3 +0,0 @@
-public static void main(String[] args) {
-    System.out.println("life is good with develop branch");
-}
